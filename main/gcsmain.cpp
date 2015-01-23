be8889aa928e0e592bedc1dc9f3618f29b10eb74
David Koo <dkoo15@gmail.com>
2015-01-23 00:26:23 -0500

    Added keyboard and mouse input functions

--- main/gcsmain.cpp
+++ main/gcsmain.cpp
@@ -7,38 +7,56 @@
 
 #include <csignal>
 #include <thread>
+#include <opencv2/opencv.hpp>
 
 //Libraries
 #include "filesource.h"
 #include "imagepacket.h"
-#include <opencv2/opencv.hpp>
+
+#define MAX_WAIT_TIME 5000
 
 uavimage impacket;
+
+void makeTarget(int event, int x, int y, int, void*){
+	if( event != cv::EVENT_LBUTTONDOWN )      return;
+
+	std::cout<<"Target Noted!"<<std::endl;
+}
 
 int main(){
 	cv::Mat jpg, image;
 	char option;
 	bool image_ok;
+	int button;
 	std::vector<unsigned char> buffer;
 	ImageSource* src;
 
 	std::cout<<"Open from files [1], or stream [2]" <<std::endl;
-	std::cin<<option;
+	std::cin >> option;
 
 
 	cv::namedWindow("main", cv::WINDOW_AUTOSIZE);
-	//if(viewoption=='1')
+
+	if(option=='1'){
 		src = new FileSource();	
-			
-	//else
- 	image_ok = src->getImage(buffer);		
+		cv::setMouseCallback("main",makeTarget,0);
+	}
+	else
+		src = new FileSource();
+	//
+ 	
+	image_ok = src->getImage(buffer);		
 	while(image_ok){
 		jpg = cv::Mat(buffer);
 		image = cv::imdecode(jpg,1);
 		cv::imshow("main", image);
+	
+		button = cv::waitKey(MAX_WAIT_TIME);
+		if(button == 'q' && option != '1' ) {
+			std::cout<<"Remember image"<<std::endl;
+		}	
+	
 		image_ok = src->getImage(buffer);
-		cv::waitKey(0);
-
 	}		
 	return 0;
 }
